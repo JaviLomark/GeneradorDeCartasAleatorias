69dfc01df39318291c31f27d2d98e8ae38e05be9
JaviLomark <lopezlarafranciscojavier@gmail.com>
2022-09-29 13:45:46 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,48 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generadorNumeros();
+  generadorPintas();
 };
+
+function generadorPintas() {
+  const pintas = ["♦", "♥", "♠", "♣"];
+  let pintasIndex = Math.floor(Math.random() * pintas.length);
+
+  if (pintas[pintasIndex] == "♥" || pintas[pintasIndex] == "♦") {
+    document.querySelector("#carta").style.color = "red";
+  } else {
+    document.querySelector("#carta").style.color = "black";
+  }
+
+  const head = (document.getElementById("head").innerHTML =
+    pintas[pintasIndex]);
+  const footer = (document.getElementById("footer").innerHTML = head);
+}
+
+function generadorNumeros() {
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numerosIndex = Math.floor(Math.random() * numeros.length);
+
+  const body = (document.getElementById("body").innerHTML =
+    numeros[numerosIndex]);
+}
+
+let boton = document.querySelector("button");
+boton.addEventListener("click", generadorNumeros);
+boton.addEventListener("click", generadorPintas);
